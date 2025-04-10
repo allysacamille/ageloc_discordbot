73ed48a9aa5d6dd4e7ef68b7a5c079b1105dcae4
allysacamille <allysachacha0706@gmail.com>
2025-04-10 22:43:33 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DISCORD_TOKEN = os.getenv("MTM1OTg4MDkxMjM0MjQ4Mjk2NA.G7abk4.1BQrZG_8rqqVAa8dWOy6x0IBGhemoSk5TDXPN4")
-MONGO_URI = os.getenv("mongodb://allysacamille:<db_password>@ac-vyrjfnz-shard-00-00.ayzdutt.mongodb.net:27017,ac-vyrjfnz-shard-00-01.ayzdutt.mongodb.net:27017,ac-vyrjfnz-shard-00-02.ayzdutt.mongodb.net:27017/?replicaSet=atlas-y7lk1m-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster1")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
 
 # Setup MongoDB
 mongo_client = AsyncIOMotorClient(MONGO_URI)
